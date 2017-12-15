32d1d1796a5a22e924ac894e2eec0e8ffea20a1c
JesseSoldat <jsoldat@hotmail.com>
2017-12-15 11:13:50 +0700

    added fonts

--- index.php
+++ index.php
@@ -16,13 +16,30 @@
 							$select_all_posts_query = mysqli_query($connection, $query);
 							
 							while($row = mysqli_fetch_assoc($select_all_posts_query)) {
-								echo $row['post_title'];
-								
-							}
-
-
-
+						
+								$post_id = $row['post_id'];
+								$post_title = $row['post_title'];
+								$post_author = $row['post_author'];
+								$post_date = $row['post_date'];
+								$post_image = $row['post_image'];
+								$post_content = substr($row['post_content'], 0, 400);
+								$post_status = $row['post_status'];
 						?>
+								<h2> 
+									<a href="post.php/<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+								</h2>
+								<p class="lead"> 
+									by <a href="author_posts.php?author=<?php echo $post_author ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author ?></a>
+								</p> 
+
+								<p>
+									<span class="glyphicon glyphicon-time">
+										<?php echo $post_date ?>
+									</span>
+									<hr> 
+								</p>
+
+						<?php } ?>
 
 							
 					</div>
